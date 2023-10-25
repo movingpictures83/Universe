d4a5c3b8f8edb56a25a325c294318eea002718dc
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-10-24 22:25:09 -0400

    Initial

---
+++ UniversePlugin.py
@@ -0,0 +1,67 @@
+import numpy as np
+from audioop import add
+
+CACHE_SIZE = 50
+PHASE_SIZE = 5000
+NUM_PHASES = 5
+UNIVERSE_SIZE = 2000
+WORKING_SET_SIZE = 30
+WS_DELTA = 0.90
+
+class UniversePlugin:
+  def input(self, inputfile):
+      pass
+  def run(self):
+      pass
+  def output(self, outputfile):
+    outfile = open(outputfile, 'w')
+    current_group = None
+    
+    other = np.arange(0,UNIVERSE_SIZE, dtype=np.int32)
+    workingSet = np.array([], dtype=np.int32)
+    
+    eps = 0.5
+    for i in range(0, NUM_PHASES) :
+        
+        
+        n = len(workingSet)
+        m = len(other)
+        
+        
+        rem_ind = np.random.rand(n)
+        add_ind = np.random.rand(m) 
+        
+        ## Remove a proportion WS_DELTA from the workingSet
+        rand = np.random.rand(len(workingSet))
+        ws_removed = workingSet[rand <= WS_DELTA] if len(workingSet) > 0 else np.array([])
+        workingSet = workingSet[rand > WS_DELTA]
+        
+        
+        ## Remove from other
+        rand = np.random.rand(len(other)) 
+        other_del = 1.0*(WORKING_SET_SIZE - len(workingSet)) / m
+        o_removed = other[rand <= other_del]
+        other = other[rand>other_del]
+
+        ## Add pages
+        workingSet = np.append(workingSet, o_removed)
+        other = np.append(other, ws_removed)
+        
+        ## Create a vertical line
+        outfile.write(str(-1))
+        outfile.write("\n")
+        
+        n = len(workingSet)
+        m = len(other)
+        
+        for _ in range(0, PHASE_SIZE) :
+        
+            if np.random.rand() < 1-eps :
+                q = workingSet[np.random.randint(0,n)]
+            else:
+                q = other[np.random.randint(0,m)]
+            outfile.write(str(q))
+            outfile.write("\n")
+    
+        
+        
